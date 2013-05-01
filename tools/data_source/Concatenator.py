d498f8c3807bfad3f1e7100b3c383dcbf92ae2d6
Matt <matthew@intersect.org.au>
2013-05-01 12:38:33 +1000

    initial commit

---
+++ tools/data_source/Concatenator.py
@@ -0,0 +1,18 @@
+import sys
+import os
+import urllib2
+file_list = sys.argv[1]
+outp = sys.argv[2]
+o = open(outp,'w')
+file_list = file_list.replace('__tc__', '')
+file_list = file_list.replace(' ', '')
+files = file_list.split("__cr____cn__")
+for f in files:
+    try:
+        content = urllib2.urlopen(f)
+        stri = unicode(content.read(), errors='ignore')
+        o.write(stri)
+        o.write('\n')
+    except:
+        pass
+o.close
