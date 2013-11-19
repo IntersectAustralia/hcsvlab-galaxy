53fa11c0d632c9401a2df530857f2f1e1bd7ecf1
matthew-intersect <matthew@intersect.org.au>
2013-11-19 11:52:06 +1100

    HCSVLAB-676 Change HCSVLAB importer tool to use API calls rather than fedora URLS

--- tools/data_source/hcsvlab_importer.py
+++ tools/data_source/hcsvlab_importer.py
@@ -1,16 +1,25 @@
 import sys
 import os
-import urllib2
+import pycurl
+import cStringIO
 file_list = sys.argv[1]
-outp = sys.argv[2]
+api_key = sys.argv[2]
+outp = sys.argv[3]
 o = open(outp,'w')
 files = file_list.split(",")
 for f in files:
-    try:
-        content = urllib2.urlopen(f)
-        stri = unicode(content.read(), errors='ignore')
-        o.write(stri)
-        o.write('\n')
-    except:
-        pass
+	try:
+		curl = pycurl.Curl()
+		buf = cStringIO.StringIO()
+		curl.setopt(curl.URL, f)
+		curl.setopt(pycurl.HTTPHEADER, ['X-API-KEY: ' + api_key, 'Accept: application/json'])
+		curl.setopt(curl.WRITEFUNCTION, buf.write)
+		curl.perform()
+		string = buf.getvalue()
+		if curl.getinfo(pycurl.HTTP_CODE) == 200:
+			o.write(string)
+			o.write('\n')
+		buf.close()
+	except:
+		pass
 o.close
