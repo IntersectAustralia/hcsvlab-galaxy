ee69c66f1831510e1d27e7516f69892b789c0d5b
elysewise2076 <elyse.wise@gmail.com>
2013-12-19 15:39:07 +1100

    HCSVLAB-687: add import results to log and concatenation functionality

--- tools/data_source/hcsvlab_data_importer.py
+++ tools/data_source/hcsvlab_data_importer.py
@@ -3,7 +3,6 @@ import os
 import pycurl
 import cStringIO
 import json
-from shutil import copyfile
 
 numFiles = sys.argv[1]
 logFile = sys.argv[2]
@@ -13,8 +12,11 @@ itemListURI = sys.argv[5]
 api_key = sys.argv[6]
 selectedTypes = sys.argv[7]
 importMetadata = sys.argv[8]
-importIndexable = sys.argv[9]
-concatenate = sys.argv[10]		
+concatenate = sys.argv[9]	
+
+#tallies of import successes/fails for logging
+importSuccessCount=0
+importFailCount=0
 
 def api_request(url):
 	curl = pycurl.Curl()
@@ -39,11 +41,19 @@ def get_json(url):
 		raise		
 
 def importData(prefix, ext, data):
-	prefix = prefix.replace("_", "-")
-	newFile = "%s/primary_%s_%s_visible_%s" % (fileDir, logFileID, prefix, ext)
-	target = open (newFile, 'w')
-	target.write(str(data))
-	target.close()
+	try:
+		prefix = prefix.replace("_", "-")
+		newFile = "%s/primary_%s_%s_visible_%s" % (fileDir, logFileID, prefix, ext)
+		target = open (newFile, 'w')
+		target.write(str(data))
+		target.close()
+		global importSuccessCount
+		importSuccessCount +=1
+	except Exception,e:
+		#do not exit if a document could not be imported. Just log the details.
+		log.write("!! Error saving document "+prefix+"."+ext+": "+ str(e))
+		global importFailCount
+		importFailCount += 1
 
 def importDocument(document):
 	docURL = document['url']
@@ -57,9 +67,11 @@ def importDocument(document):
 	except Exception,e:
 		#do not exit if a document could not be imported. Just log the details.
 		log.write("!! Error importing document "+docURL+": "+ str(e))
+		global importFailCount
+		importFailCount += 1
 
 documentsList =[]
-indexableDocumentsList =[]
+concatenateList =[]
 metadataList =[]
 
 log = open (logFile, 'a')
@@ -70,7 +82,6 @@ log.write("\t API-KEY: "+api_key+"\n")
 log.write("\t Job Name: "+logFile+"\n")
 log.write("\t Import File Types: "+selectedTypes+"\n")
 log.write("\t Import Metadata: "+importMetadata+"\n")
-log.write("\t Import Indexed Documents: "+importIndexable+"\n")
 log.write("\t Concatenate Text Files: "+concatenate+"\n\n")
 
 try:
@@ -81,42 +92,51 @@ try:
 	log.write("This item list contains "+str(len(itemURIs))+" items.\n\n")
 
 	for itemURI in itemURIs:
-		log.write("---- ITEM: " +str(itemURI) + "\n")
+		log.write("ITEM: " +str(itemURI) + "\n")
 		itemResponse =  get_json(url=(itemURI).encode('ascii','ignore'))
 		itemName = (str(itemURI)).split("/")[-1]
 
-		#add the documents/metadata associated with this item
+		#collect the documents/metadata to import
 		if importMetadata == "true":
-			log.write("metadata document: "+(str(itemResponse['metadata']))[:100]+"...\n")
+			content = itemResponse['metadata']
+			log.write("---- metadata document: "+(str(content))[:100]+"...\n")
 			metadataItem = {'name':itemName }
-			metadataItem['content'] = itemResponse['metadata']
+			metadataItem['content'] = content
 			metadataList.append(metadataItem)
-		if importIndexable  == "true" and (str(itemResponse['primary_text_url']) != "No primary text found"):
-			log.write("indexable document: "+(str(itemResponse['primary_text_url']))+"\n")
+		if concatenate == "true" and (str(itemResponse['primary_text_url']) != "No primary text found"):
 			indexableItem = {'name':itemName}
 			indexableItem['url'] = itemResponse['primary_text_url']
-			indexableDocumentsList.append(indexableItem)
-		else:
-			docs = itemResponse['documents']
-			for doc in docs: 
-				if doc['dc:type'] in selectedTypes:
-					log.write("document: "+str(doc['url'])+"\n")
+			concatenateList.append(indexableItem)
+		docs = itemResponse['documents']
+		for doc in docs: 
+			if doc['dc:type'] in selectedTypes:
+				log.write("---- document: "+str(doc['url'])+"\n")
+				if (concatenate == "false" or doc['dc:type'] != "Text"):
 					documentsList.append(doc)
-		log.write("\n\n")			
+	log.write("\n\n")				
 
-	# TODO: concatenation
-		# get every document with 'text' type (remove from list)
-		# do concatenation	
-
+	#perform imports
 	for document in documentsList:
 		importDocument(document)
-	for indexableItem in indexableDocumentsList:
-		status, content = api_request( url=(indexableItem['url']).encode('ascii','ignore'))
-		importData(indexableItem['name']+' indexable-document', 'txt', content)	
 	for metadataItem in metadataList:
-		importData(metadataItem['name']+' metadata-document', 'txt', metadataItem['content'])	
+		content = json.dumps(metadataItem['content'], indent=4)
+		importData(metadataItem['name']+' metadata-document', 'txt', content)
+	if concatenate=="true":
+		concatenatedContent =""
+		for textItem in concatenateList:
+			try:
+				status, content = api_request( url=(textItem['url']).encode('ascii','ignore'))
+				concatenatedContent = concatenatedContent +"\n\n"+str(content)
+			except Exception, e:
+				log.write("!! Error importing document "+str(textItem['url'])+": "+ str(e)+"\n")
+		if concatenatedContent != "":
+			importData('concatenated texts', 'txt', concatenatedContent)
 
+	#log the results
+	log.write(str(importSuccessCount)+" document(s) were successfully imported.\n")
+	log.write(str(importFailCount)+" document(s) could not be imported due to errors.\n")
 	log.close()
+
 except Exception,e:
     log.write("ERROR: "+str(e)+"\n")
     raise
