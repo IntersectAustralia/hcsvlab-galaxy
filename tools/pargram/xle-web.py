18dc26aba1188398be2ab82f22718b6b04d01275
Gabriel E. Gasser Noblia <gabriel@intersect.org.au>
2014-01-09 14:29:23 +1100

    HCSVLAB-698: Fixed HTTP GET restriction in request size by using POST. Also made XLE-Web tool to run Parse Sentence automatically.

--- tools/pargram/xle-web.py
+++ tools/pargram/xle-web.py
@@ -12,8 +12,25 @@ text = open(inp,'r').read()
 outp = sys.argv[2]
 
 # Define Indinesian as pre-selected grammar
-params = urllib.urlencode({'grammar': 'Indonesian', 'sentence': text})
-data="<div width='100%' height='100%'><iframe src='" + XLE_WEB_URL + "?" + params + "' width='100%' height='100%' frameborder='0'></iframe></div>"
+data="""
+    <div width='100%' height='100%'>
+        <iframe name='xle-web-frame' src='' width='100%' height='100%' frameborder='0'></iframe>
+    </div>
+
+    <form id='xle-web-form' action='##_XLE_WEB_URL_##' target='xle-web-frame' method='POST' style='display:none'>
+        <input type='text' name='grammar' value='##_GRAMMAR_##'/>
+        <input type='text' name='sentence' value="##_SENTENCE_##"/>
+        <input type='text' name="parse-sentence" value="Parse sentence">
+    </form>
+
+    <script type="text/javascript">
+        document.getElementById('xle-web-form').submit();
+    </script>
+"""
+
+data = data.replace('##_XLE_WEB_URL_##', XLE_WEB_URL)
+data = data.replace('##_GRAMMAR_##', 'Indonesian')
+data = data.replace('##_SENTENCE_##', text.replace('"', '\"'))
 
 # write result in the output file
 o = open(outp,'w')
