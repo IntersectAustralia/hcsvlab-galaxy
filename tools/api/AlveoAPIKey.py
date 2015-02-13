d458e88b6ec629c2832391dc0612dafb7b1ff8f4
Ilya Anisimoff <anisimoff@gmail.com>
2015-02-13 16:25:37 +1100

    HCSVLAB-966: Adds API key storage tool

---
+++ tools/api/AlveoAPIKey.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+import argparse
+import pyalveo
+import sys
+
+API_URL = 'https://app.alveo.edu.au'
+
+def parser():
+    parser = argparse.ArgumentParser(description="retrieve a Alveo itemlists")
+    parser.add_argument('--api_key', required=True, action="store", type=str, help="Alveo API key")
+    parser.add_argument('--output_path', required=True, action="store", type=str, help="File to store the API key in")
+    return parser.parse_args()
+
+
+def main():
+    args = parser()
+    try:
+        client = pyalveo.Client(api_key=args.api_key, api_url=API_URL)
+    except pyalveo.APIError as e:
+        print("ERROR: " + str(e), file=sys.stderr)
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
