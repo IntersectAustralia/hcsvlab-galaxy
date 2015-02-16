8d19b333ffefffe6db51abb383c942d62f01e003
Ilya Anisimoff <ilya@intersect.org.au>
2015-02-16 15:02:59 +1100

    MISC: Updates Alveo Item List Importer error handling

--- tools/data_source/AlveoItemListImporter.py
+++ tools/data_source/AlveoItemListImporter.py
@@ -1,36 +1,39 @@
+from __future__ import print_function
 import json
 import argparse
 import pyalveo
+
 
 API_URL = 'https://app.alveo.edu.au'
 
 
 def parser():
-    the_parser = argparse.ArgumentParser(description="retrieve a Alveo itemlists")
-    the_parser.add_argument('--api_key', required=True, action="store", type=str, help="Alveo API key")
-    the_parser.add_argument('--output', required=True, action="store", type=str, help="output file path")
-    return the_parser.parse_args()
+    parser = argparse.ArgumentParser(description="Retrieves Alveo Item Lists")
+    parser.add_argument('--api_key', required=True, action="store", type=str, help="Alveo API key")
+    parser.add_argument('--output', required=True, action="store", type=str, help="Path to output file")
+    return parser.parse_args()
 
 
 def get_item_lists(api_key):
-    item_lists = None
-    try:
-        client = pyalveo.Client(api_key=api_key, api_url=API_URL)
-        item_lists = client.get_item_lists()
-    except pyalveo.APIError as e:
-        # log.write("ERROR: "+str(e)+"\n")
-        pass
-    return item_lists
+    client = pyalveo.Client(api_key=api_key, api_url=API_URL)
+    return client.get_item_lists()
 
 def write_table(item_lists, filename):
     with open(filename, 'w') as outfile:
         for list_set in item_lists.itervalues():
             for item_list in list_set:
-                print item_list
                 outfile.write("%s (%d)\t%s\n" % (item_list['name'], item_list['num_items'], item_list['item_list_url']))
 
-if __name__ == '__main__':
+def main():
     args = parser()
-    item_lists = get_item_lists(args.api_key)
-    if item_lists:
-        write_table(item_lists, args.output)
+    try:
+        item_lists = get_item_lists(args.api_key)
+        if item_lists:
+            write_table(item_lists, args.output)
+    except pyalveo.APIError as e:
+        print("ERROR: " + str(e), file=sys.stderr)
+        sys.exit(1)
+    
+
+if __name__ == '__main__':
+    main()
