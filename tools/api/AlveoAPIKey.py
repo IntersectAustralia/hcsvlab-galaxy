8b37101caa6bfb1a0c2fe3ea3fcf8bfe4bd3ef45
Ilya Anisimoff <ilya@intersect.org.au>
2015-02-16 15:32:06 +1100

    HCSVLAB-966: Fixes API key persistance

--- tools/api/AlveoAPIKey.py
+++ tools/api/AlveoAPIKey.py
@@ -6,7 +6,7 @@ import sys
 API_URL = 'https://app.alveo.edu.au'
 
 def parser():
-    parser = argparse.ArgumentParser(description="retrieve a Alveo itemlists")
+    parser = argparse.ArgumentParser(description="Retrieves Alveo Item Lists")
     parser.add_argument('--api_key', required=True, action="store", type=str, help="Alveo API key")
     parser.add_argument('--output_path', required=True, action="store", type=str, help="File to store the API key in")
     return parser.parse_args()
@@ -16,6 +16,9 @@ def main():
     args = parser()
     try:
         client = pyalveo.Client(api_key=args.api_key, api_url=API_URL)
+        outfile = open(args.output_path, 'w')
+        outfile.write(args.api_key)
+        outfile.close()
     except pyalveo.APIError as e:
         print("ERROR: " + str(e), file=sys.stderr)
         sys.exit(1)
