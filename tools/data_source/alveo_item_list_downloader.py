d64a0ce4f6d52ccb9c17de73450305e20007dde7
Ilya Anisimoff <ilya@intersect.org.au>
2015-02-26 13:56:04 +1100

    HCSVLAB-966: Adds doc strings to item list downloader

--- tools/data_source/alveo_item_list_downloader.py
+++ tools/data_source/alveo_item_list_downloader.py
@@ -5,9 +5,7 @@ import pyalveo
 import sys
 import os
 
-
-API_URL = 'https://app.alveo.edu.au'
-
+API_URL = 'https://app.alveo.edu.au' # TODO: export constants to a separate module
 
 def parser():
     parser = argparse.ArgumentParser(description="Downloads documents in an Alveo Item List")
@@ -17,12 +15,20 @@ def parser():
     parser.add_argument('--output_path', required=True, action="store", type=str, help="Path to output file")
     return parser.parse_args()
 
-
 def get_item_list(api_key, item_list_url):
     client = pyalveo.Client(api_key=api_key, api_url=API_URL)
     return client.get_item_list(item_list_url)
 
 def filter_documents_by_type(item_list, doc_types):
+    """Filters and Item List to the specified document types.
+
+    :type item_list: pyalveo.ItemList
+    :param item_list: Item List to filter
+
+    :type doc_types: list of String
+    :param doc_types: pyalveo.Document types to filter from the set
+        [Audio, Original, Raw, Text, TextGrid, Video, unspecified]
+    """
     items = item_list.get_all()
     filtered_documents = []
     for item in items:
@@ -31,6 +37,15 @@ def filter_documents_by_type(item_list, doc_types):
     return filtered_documents
 
 def download_documents(documents, output_path):
+    """
+    Downloads a list of documents to the directory specificed by output_path.
+
+    :type documents: list of pyalveo.Document
+    :param documents: Documents to download
+
+    :type output_path: String
+    :param output_path: directory to download to the documents to
+    """
     for document in documents:
         if not os.path.exists(output_path):
             os.makedirs(output_path)
