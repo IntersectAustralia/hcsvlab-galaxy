f367543e77d5ca1ca65b68692be7f72dfa1b9919
Shaun Muscat <shaun@intersect.org.au>
2015-08-03 17:31:17 +1000

    HCSVLAB-1003: data importer creates log and outputs item list items

--- tools/data_source/AlveoDataImporter.py
+++ tools/data_source/AlveoDataImporter.py
@@ -13,7 +13,10 @@ def parser():
     parser.add_argument('--api_key', required=True, action="store", type=str, help="Alveo API key")
     parser.add_argument('--item_list_url', required=True, action="store", type=str, help="Item List to download")
     parser.add_argument('--doc_types', required=True, action="store", type=str, help="Item types to download")
-    parser.add_argument('--output_path', required=True, action="store", type=str, help="Path to output file")
+    parser.add_argument('--output_path', required=True, action="store", type=str, help="Path to output files")
+    parser.add_argument('--output_log', required=True, action="store", type=str, help="Path to output log file")
+    parser.add_argument('--metadata', required=True, action="store", type=str, help="Import metadata")
+    parser.add_argument('--concat', required=True, action="store", type=str, help="Concatenate all text documents into one galaxy document")
     return parser.parse_args()
 
 # TODO: export common function to helper module
@@ -37,6 +40,17 @@ def download_documents(documents, output_path):
             os.makedirs(output_path)
         document.download_content(output_path)
 
+def write_log(args):
+    log = open (args.output_log, 'a')      
+    log.write("Alveo Import Tool.\n\n")        
+    log.write("User input parameters are as follows: \n")      
+    log.write("\t Item List URI: "+args.item_list_url+"\n")       
+    log.write("\t API-KEY: "+args.api_key+"\n")     
+    log.write("\t Job Name: "+args.output_log+"\n")        
+    log.write("\t Import File Types: "+args.doc_types+"\n")     
+    log.write("\t Import Metadata: "+args.metadata+"\n")      
+    log.write("\t Concatenate Text Files: "+args.concat+"\n\n")
+
 def main():
     args = parser()
     try:
@@ -44,6 +58,7 @@ def main():
         doc_types = args.doc_types.split(',')
         documents = filter_documents_by_type(item_list, doc_types)
         download_documents(documents, args.output_path)
+        write_log(args)
     except pyalveo.APIError as e:
         print("ERROR: " + str(e), file=sys.stderr)
         sys.exit(1)
