b312c201c4eece5f8ba7f43658d10c982b478ba4
Shaun Muscat <shaun@intersect.org.au>
2015-08-07 13:51:31 +1000

    HCSVLAB-1094: revert frequency tool back to not accept POS data

--- tools/nltk_tools/g_frequency.py
+++ tools/nltk_tools/g_frequency.py
@@ -6,22 +6,16 @@ def arguments():
   parser = argparse.ArgumentParser(description="generate a word frequency table from a text")
   parser.add_argument('--input', required=True, action="store", type=str, help="input text file")
   parser.add_argument('--output', required=True,  action="store", type=str, help="output file path")
-  parser.add_argument('--pos', required=True,  action="store", type=str, help="output file path")
   args = parser.parse_args()
   return args
 
 
-def frequency(in_file, out_file, pos):
+def frequency(in_file, out_file):
     """Input: a text file
     Output: a table of word frequency with three columns for Word, Count and Percent frequency
     """
     text = unicode(open(in_file, 'r').read(), errors='ignore')
-    if pos == "true":
-      text = text.split(' ')[:-1]
-      words = [x[0:x.index('/')] if x != '\n' else x for x in text]
-      words = [x.lower().strip(' ').strip('\n') for x in words]
-    else:
-      words = nltk.word_tokenize(text)
+    words = nltk.word_tokenize(text)
     frequency = FreqDist(words)
     total = float(frequency.N())
     output = open(out_file, 'w')
@@ -33,6 +27,6 @@ def frequency(in_file, out_file, pos):
 
 if __name__ == '__main__':
     args = arguments()
-    frequency(args.input, args.output, args.pos)
+    frequency(args.input, args.output)
 
 
